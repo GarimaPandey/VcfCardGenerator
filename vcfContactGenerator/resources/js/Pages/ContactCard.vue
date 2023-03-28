236fdadd31ef58190abc0fb65dc9b35ec2ce971d
Garima <gpandey@unomaha.edu>
2023-03-28 12:24:24 -0400

    data rendered to vue component

--- vcfContactGenerator/resources/js/Pages/ContactCard.vue
+++ vcfContactGenerator/resources/js/Pages/ContactCard.vue
@@ -1,12 +1,20 @@
 
 @extends('app')
 
-<template>
-  <div>
-    <ul>
-        {{user}}
-        
-    </ul>
+<template id="cc">
+  <div class="user-card">
+    <div class="user-card-header">
+      <img :src="user.mug" alt="User Mugshot" />
+      <div class="user-card-header-info">
+        <h1>{{ user.first_name }} {{ user.last_name }}</h1>
+        <p>{{ user.circle }}</p>
+      </div>
+    </div>
+    <div class="user-card-body">
+      <p>Email: {{ user.email }}</p>
+      <p>Phone: {{ user.phone }}</p>
+      <p>Roles: {{ user.roles.join(', ') }}</p>
+    </div>
   </div>
 </template>
 
@@ -17,3 +25,47 @@
         }
     }
 </script>
+<style>
+
+.user-card {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  overflow: hidden;
+  width: 300px;
+}
+
+.user-card-header {
+  display: flex;
+  align-items: center;
+  background-color: #f5f5f5;
+  padding: 10px;
+}
+
+.user-card-header img {
+  border-radius: 50%;
+  height: 80px;
+  margin-right: 10px;
+  width: 80px;
+}
+
+.user-card-header-info h1 {
+  font-size: 24px;
+  margin: 0;
+}
+
+.user-card-header-info p {
+  color: #666;
+  font-size: 18px;
+  margin: 0;
+}
+
+.user-card-body {
+  padding: 10px;
+}
+
+.user-card-body p {
+  font-size: 16px;
+  margin: 5px 0;
+}
+</style>
